88e60b526e53cbf2556296e33b99546f6cd3a292
Adib_Bh <121905691+Adib-96@users.noreply.github.com>
2026-02-18 13:11:57 +0100

    typo fix

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 function createServerSupabase() {
   return createClient(
-    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_DB_URL!,
     process.env.SUPABASE_PUBLISHABLE_DEFAULT_KEY!
   )
 }
